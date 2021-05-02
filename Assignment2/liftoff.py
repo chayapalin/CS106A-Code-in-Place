954f03860822215a38dede32b79230268b0c81af
chayapalin <68495962+chayapalin@users.noreply.github.com>
2021-05-02 15:26:06 +0700

    Update liftoff.py

--- Assignment2/liftoff.py
+++ Assignment2/liftoff.py
@@ -7,13 +7,11 @@ It counts down the numbers from 10 to 1 and then writes “Liftoff!”
 
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+    count = 10
+    for i in range(10):
+        print(str(count))
+        count -= 1
+    print("Liftoff!")
 
 
 # This provided line is required at the end of a Python file
