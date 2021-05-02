5b084852a4045b0ddb135492fbfd9c1cc593b4cd
chayapalin <68495962+chayapalin@users.noreply.github.com>
2021-05-02 15:25:02 +0700

    Update subtract_numbers.py

--- Assignment2/subtract_numbers.py
+++ Assignment2/subtract_numbers.py
@@ -7,13 +7,11 @@ the first number minus the second number.
 
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+    print("This program subtracts one number from another.")
+    first_number = float(input("Enter first number: "))
+    second_number = float(input("Enter second number: "))
+    result = first_number - second_number
+    print("The result is", result)
 
 
 # This provided line is required at the end of a Python file
