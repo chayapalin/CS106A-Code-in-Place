6d32f0b83ed86992170fa86ecc5cd291b5be7ce5
chayapalin <68495962+chayapalin@users.noreply.github.com>
2021-05-02 15:25:40 +0700

    Update random_numbers.py

--- Assignment2/random_numbers.py
+++ Assignment2/random_numbers.py
@@ -5,17 +5,17 @@ This program prints a series of random numbers in the
 range from MIN_RANDOM to MAX_RANDOM, inclusive
 """
 
+NUM_RANDOM = 10
+MIN_RANDOM = 0
+MAX_RANDOM = 100
+
 import random
 
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+    for i in range(NUM_RANDOM):
+        random_number = random.randint(MIN_RANDOM, MAX_RANDOM)
+        print(random_number)
 
 
 # This provided line is required at the end of a Python file
