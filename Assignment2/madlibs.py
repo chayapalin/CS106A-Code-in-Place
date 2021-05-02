386cdfc952154e915555206489c19a13473d6984
chayapalin <68495962+chayapalin@users.noreply.github.com>
2021-05-02 15:24:27 +0700

    Update madlibs.py

--- Assignment2/madlibs.py
+++ Assignment2/madlibs.py
@@ -18,11 +18,7 @@ def main():
     print(WIZARD + " realized they couldn't keep all those " + FRUIT + " to themselves,")
     print("so they sold them at the market for $" + str(PRICE) + " apiece,")
     print("and with the earnings bought fruit to share with the entire village!")
-
-
-if __name__ == '__main__':
-    main()
-
+    print("Legend says " + str(YEARS) + " years later, " + WIZARD + " is still eating fruit.")
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
